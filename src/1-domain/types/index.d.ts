c1bc7b11ca303c7165788e3d8addb9ead05321a9
cluxnei <cluxnei31@gmail.com>
2024-02-19 09:43:27 -0300

    chore: add custom generics types

---
+++ src/1-domain/types/index.d.ts
@@ -0,0 +1,4 @@
+export type AnyObject = Record<string, any>;
+export type CustomObject<T extends any> = Record<string, T>;
+export type NumberObject = Record<string, number>;
+export type StringObject = Record<string, string>;
